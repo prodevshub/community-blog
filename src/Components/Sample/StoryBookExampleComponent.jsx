f76440099b619b2d1720f20e8168a4c2fb2ef7a9
MarcinkEve <marcinke@gmail.com>
2022-05-22 23:05:27 +0300

    trying SB args options

--- src/Components/Sample/StoryBookExampleComponent.jsx
+++ src/Components/Sample/StoryBookExampleComponent.jsx
@@ -1,13 +1,17 @@
 import React from "react";
-import logo from "../logo.svg";
-import "../App.css";
+import logo from "../../logo.svg";
+import "../../App.css";
 
+//cia priimam propsus i komponenta is stories
+function StoryBookExampleComponent({footer, bgColor, handleOnChange, color, backgroundColor, fontSize, fontWeight, textAlign, primary, value, onClick}) {
 
-function StoryBookExampleComponent() {
   return (
     <>
-      <h1>Sample component in community blog</h1>
+      <h1 style={{backgroundColor, color, fontSize, fontWeight, textAlign}}>Sample component in community blog</h1>
       <img src={logo} className="App-logo" alt="logo" />
+      <button style={{ backgroundColor}} bgColor={bgColor} primary={primary} value={value} onClick={handleOnChange}>I am Mr.Button</button>
+      <img src={logo} className="App-logo" alt="logo" />
+      <footer >{footer}</footer>
     </>
   );
 }
--- src/Components/Sample/StoryBookExampleComponent.stories.jsx
+++ src/Components/Sample/StoryBookExampleComponent.stories.jsx
@@ -1,58 +1,87 @@
-import React from "react";
+import { useState } from "react";
 import StoryBookExampleComponent from "./StoryBookExampleComponent";
 
-
 export default {
-title: "Example component",
-component: StoryBookExampleComponent,
-argTypes: {
-    backgroundColor: { control: 'color' },
+  title: "Example component",
+  component: StoryBookExampleComponent,
+  argTypes: {
+    backgroundColor: { control: "color" },
   },
-
-}
-
-const Template = (args) => <StoryBookExampleComponent {...args} />;
-
-// export const FirstStory = {
-//     args: {
-//     },
-//   };
-
-  export const Primary = Template.bind({});
-Primary.args = {
-  primary: true,
-  label: 'logo',
 };
 
+//cia aprasomas sablonas kaip bus atvaizduojamas komponentas:
+//propsus paduodame i f-ja kaip argumentus (args) visus iskart arba pavieniuis(uzkomentuotos eilutes)
+const Template = (args) => {
+  // const Template = ({color, backGround}}) => {
+  return (
+    <div>
+      <StoryBookExampleComponent {...args} />
+      {/* <StoryBookExampleComponent color={color} backgroundColor={backgroundColor} /> */}
+    </div>
+  );
+};
 
+//paduodamos skirtingos argumentu kombinacijos storiams, kurie naudoja ta sablona:
+export const Primary = Template.bind({});
+Primary.args = {
+  primary: true,
+  label: "Labas",
+  color: "red",
+  backgroundColor: "lightblue",
+};
 
+export const Labas = Template.bind({});
+Labas.args = {
+  fontSize: "50px",
+  fontWeight: "900",
+  textAlign: "center",
+  backgroundColor: "lightblue",
+};
 
+//reusing previous story's args in another
+export const DarVienas = Template.bind({});
+DarVienas.args = {
+  ...Primary.args,
+  fontSize: "50px",
+  fontWeight: "900",
+  textAlign: "center",
+  backgroundColor: "lightblue",
+};
 
+//modifikuojamas vienas komponento elementas(footer)
+const TemplateAspect1 = (args) => (
+  <StoryBookExampleComponent {...args}>
+    <footer>{args.footer}</footer>
+  </StoryBookExampleComponent>
+);
 
+export const CustomFooter = TemplateAspect1.bind({});
+CustomFooter.args = {
+  footer: "Built with Storybook- args modified aspect of component",
+};
 
+//neveikia :(
+export const PrimarySecondary = () => {
+  // Sets the hooks for both the label and primary props
+  const [value, setValue] = useState("Secondary");
+  const [bgColor, setBgColor] = useState("pink");
+  const [isPrimary, setIsPrimary] = useState(false);
 
-
-
-
-
-
-
-
-
-
-
-
-
-//TUOJ  GRĮŠIU ;)
-
-
-
-
-
-
-
-
-
-
-
-
+  // Sets a click handler to change the label's value
+  const handleOnChange = () => {
+    if (!isPrimary) {
+      setIsPrimary(true);
+      setValue("Primary");
+      setBgColor("green");
+    }
+  };
+  return (
+    <StoryBookExampleComponent
+      bgColor={bgColor}
+      primary={isPrimary}
+      onChange={handleOnChange}
+    >
+      {value}
+    </StoryBookExampleComponent>
+  );
+};
