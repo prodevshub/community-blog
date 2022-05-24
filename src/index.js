e9002c8d25c3377d1087859c4ba4c82fac0496fa
SwallowEve <swalloweve@gmail.com>
2022-05-24 12:30:29 +0300

    PROD-13 installed Sass, fixed APP.jsx in import

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './App.jsx';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+    // eslint-disable-next-line react/jsx-filename-extension
     <React.StrictMode>
         <App />
     </React.StrictMode>
