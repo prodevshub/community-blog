6e6000867093c4b0ab09081426c434dd20bb2228
MarcinkEve <marcinke@gmail.com>
2022-05-25 20:23:55 +0300

    almost finished

--- src/Components/StoryBookExampleComponent/StoryBookExampleComponent.stories.jsx
+++ src/Components/StoryBookExampleComponent/StoryBookExampleComponent.stories.jsx
@@ -4,30 +4,39 @@ import StoryBookExampleComponent from "./StoryBookExampleComponent";
 export default {
   title: "Example component",
   component: StoryBookExampleComponent,
-  argTypes: {
-    backgroundColor: { control: "color" },
-  },
+  // argTypes: {
+  //   background: { control: "color" },
+  //   active: { control: 'boolean' }
+  // },
 };
 
 //cia aprasomas sablonas kaip bus atvaizduojamas komponentas:
 //propsus paduodame i f-ja kaip argumentus (args) visus iskart arba pavieniuis(uzkomentuotos eilutes)
 const Template = (args) => {
-  // const Template = ({color, backGround}}) => {
+  // const Template = ({color, background}}) => {
   return (
     <div>
       <StoryBookExampleComponent {...args} />
-      {/* <StoryBookExampleComponent color={color} backgroundColor={backgroundColor} /> */}
+      {/* <StoryBookExampleComponent color={color} background={background} /> */}
     </div>
   );
 };
 
 //paduodamos skirtingos argumentu kombinacijos storiams, kurie naudoja ta sablona:
+export const Arguments = Template.bind({});
+Arguments.args = {
+  primary: true,
+  label: "Laba diena",
+  color: "yellow",
+  background: "black",
+  padding: "15px"
+};
 export const Primary = Template.bind({});
 Primary.args = {
   primary: true,
   label: "Labas",
   color: "red",
-  // backgroundColor: "black",
+  // background: "black",
 };
 
 export const Labas = Template.bind({});
@@ -37,6 +46,25 @@ Labas.args = {
   textAlign: "right",
   backgroundColor: "green",
 };
+
+
+//naudojami dekoratoriai
+export const Decorators = Template.bind({});
+Decorators.decorators = [
+  (Story) => (
+    <div
+      style={{
+        fontSize: "50px",
+        fontWeight: "900",
+        textAlign: "right",
+        backgroundColor: "green",
+        margin: "15px"
+      }}
+    >
+      <StoryBookExampleComponent />
+    </div>
+  ),
+];
 
 //reusing previous story's args in another
 export const DarVienas = Template.bind({});
@@ -59,27 +87,39 @@ CustomFooter.args = {
   footer: "Built with Storybook- args modified aspect of component",
 };
 
-//neveikia :(
-export const PrimarySecondary = () => {
+//naudojamas hookas "useState"
+export const PrimarySecondary = ({primary, label, name, backgroundColor, color}) => {
   // Sets the hooks for both the label and primary props
   const [value, setValue] = useState("Secondary");
-  const [bgColor, setBgColor] = useState("pink");
-  const [isPrimary, setIsPrimary] = useState(false);
+   const [isPrimary, setIsPrimary] = useState(false);
 
   // Sets a click handler to change the label's value
   const handleOnChange = () => {
     if (!isPrimary) {
       setIsPrimary(true);
       setValue("Primary");
-      setBgColor("green");
     }
   };
+  
   return (
     <StoryBookExampleComponent
-      bgColor={bgColor}
-      primary={isPrimary}
-      handleOnChange={handleOnChange}
-      label={value}
+    handleOnChange={handleOnChange}
+    label={value}
+      // primary={isPrimary}
+      // name={name}  zemiau aprasom ta pati propsu padavima tik su mazesniu teksto kiekiu:
+      {...{
+        name,
+        primary: isPrimary,
+        backgroundColor,
+        color
+      }}
     ></StoryBookExampleComponent>
   );
 };
+PrimarySecondary.args = {
+  primary: true,
+  label: "Primary",
+  name: "I am mister button",
+  backgroundColor: "black", 
+  color: "yellow"
+}
