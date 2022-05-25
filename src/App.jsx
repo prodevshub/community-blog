f826672d6550677008752af9e8a5b4def5fb43d4
SwallowEve <swalloweve@gmail.com>
2022-05-25 20:59:20 +0300

    PROD-13 styles applied, animation and background

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,78 @@
 import React from 'react';
 import './App.css';
+import './App.scss';
+import Header from './Components/Header/Header.jsx';
+import sticky from './Functions/Sticky';
 
 function App() {
     const name = 'Martynas';
+    window.onscroll = () => sticky();
     return (
         <div className="App">
-            <h1>Hello World</h1>
+            <Header />
+            <main>
+                <h1>Hello World</h1>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Recusandae, aspernatur excepturi itaque consequatur veniam officia impedit eius ducimus
+                    doloremque aut beatae, quisquam velit provident sapiente natus sit quae alias sequi! 12000
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Recusandae, aspernatur excepturi itaque consequatur veniam officia impedit eius ducimus
+                    doloremque aut beatae, quisquam velit provident sapiente natus sit quae alias sequi! 12000
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Recusandae, aspernatur excepturi itaque consequatur veniam officia impedit eius ducimus
+                    doloremque aut beatae, quisquam velit provident sapiente natus sit quae alias sequi! 12000
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Recusandae, aspernatur excepturi itaque consequatur veniam officia impedit eius ducimus
+                    doloremque aut beatae, quisquam velit provident sapiente natus sit quae alias sequi! 12000
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Recusandae, aspernatur excepturi itaque consequatur veniam officia impedit eius ducimus
+                    doloremque aut beatae, quisquam velit provident sapiente natus sit quae alias sequi! 12000
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Recusandae, aspernatur excepturi itaque consequatur veniam officia impedit eius ducimus
+                    doloremque aut beatae, quisquam velit provident sapiente natus sit quae alias sequi! 12000
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Recusandae, aspernatur excepturi itaque consequatur veniam officia impedit eius ducimus
+                    doloremque aut beatae, quisquam velit provident sapiente natus sit quae alias sequi! 12000
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Recusandae, aspernatur excepturi itaque consequatur veniam officia impedit eius ducimus
+                    doloremque aut beatae, quisquam velit provident sapiente natus sit quae alias sequi! 12000
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Recusandae, aspernatur excepturi itaque consequatur veniam officia impedit eius ducimus
+                    doloremque aut beatae, quisquam velit provident sapiente natus sit quae alias sequi! 12000
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Recusandae, aspernatur excepturi itaque consequatur veniam officia impedit eius ducimus
+                    doloremque aut beatae, quisquam velit provident sapiente natus sit quae alias sequi! 12000
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Recusandae, aspernatur excepturi itaque consequatur veniam officia impedit eius ducimus
+                    doloremque aut beatae, quisquam velit provident sapiente natus sit quae alias sequi! 12000
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Recusandae, aspernatur excepturi itaque consequatur veniam officia impedit eius ducimus
+                    doloremque aut beatae, quisquam velit provident sapiente natus sit quae alias sequi! 12000
+                </p>
+            </main>
         </div>
     );
 }
---
+++ src/Functions/Sticky.js
@@ -0,0 +1,10 @@
+function sticky() {
+    const header = document.querySelector('.header');
+    const stick = header.offsetTop;
+    if (window.pageYOffset > stick) {
+        header.classList.add('sticky');
+    } else {
+        header.classList.remove('sticky');
+    }
+}
+export default sticky;
