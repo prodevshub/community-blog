64adfa7f77bbef97ea860587a7c4da23b1898d92
Darius Navickas <dariusnaw@gmail.com>
2022-05-26 20:51:42 +0300

    PROD-14 Created Footer.jsx component and scss file

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Footer.scss";
+
+function Footer() {
+	return (
+		<footer>
+			<div className="footer_main_box">
+				<span>Copyright © 2022 community blog. All Rights Reserved</span>
+			</div>
+		</footer>
+	);
+}
+
+export default Footer;
