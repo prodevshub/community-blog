8f4edb3643bbc9add8d998a00f0cd93e36407090
MrStonkus <valdas@stonkus.lt>
2022-05-29 12:28:12 +0300

    Add default parameters to Button component

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './Button.scss'
 
 const Button = ({
-    className, onClick, children, ariaLabel
+    className = 'cust-btn cust-btn-primary', onClick = null, children = 'I am button', ariaLabel = 'I am button'
 }) => (
     <button className={className} onClick={onClick} aria-label={ariaLabel}>{children}</button>
 )
