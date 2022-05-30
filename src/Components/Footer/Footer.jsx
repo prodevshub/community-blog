b062e28e3a28f3aa2677c10bfeaa6bd383fd04ce
Darius Navickas <dariusnaw@gmail.com>
2022-05-30 22:24:32 +0300

    Added readme & edited footer disign

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
 import './Footer.scss'
 
-const Footer = () => {
-	return (
-		<footer className='footer'>
-			<div className='footer_main_box'>
-				<div className='footer_first_block'>
-					<h2>Title</h2>
-					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, pariatur!</p>
-				</div>
-				<div className='footer_second_block'></div>
-			</div>
-			<span>Copyright © 2022 community blog. All Rights Reserved</span>
-		</footer>
-	)
-}
+const Footer = () => (
+    <footer className="footer">
+        <div className="footer_main_box">
+            <div className="footer_first_block">
+                <h3>Lucky</h3>
+                <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, pariatur!</span>
+            </div>
+            <div className="footer_second_block">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, impedit!</p>
+            </div>
+        </div>
+        <div className="footer_copyright">
+            <span>Copyright © 2022 community blog. All Rights Reserved</span>
+        </div>
+    </footer>
+)
 export default Footer
