bd0c9276ede0e3efabdb156777fef6bb07ef698f
Aronwebpro <aronwebpro@gmail.com>
2022-06-08 22:35:42 +0300

    contenful init

---
+++ src/api/contenful.js
@@ -0,0 +1,18 @@
+// const contentful = require('contentful')
+//
+// class Contenful {
+//     constructor() {
+//         this.client = contentful.createClient({
+//             space: 'x7a0w5de7yy0',
+//             accessToken: 'qEOHgxo4QWL1l8G0ZqfldEjfqYlobV2CZBry6sG3Lgw'
+//         })
+//     }
+//
+//     getPosts = async () => {
+//         const posts = await this.client.getEntry('2Ko2menhaf1g79qMj49JV5')
+//         // eslint-disable-next-line no-console
+//         console.log(posts)
+//     }
+// }
+//
+// export default new Contenful()
