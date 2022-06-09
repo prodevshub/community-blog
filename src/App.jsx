e2771ac357fb3e85c152328faf0b7c6c66d85c1b
MartynasKupsys <martynas.kupsys13@gmail.com>
2022-06-09 14:04:38 +0300

    Message component styling added readme

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,25 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import './App.scss'
-import Message from './Components/Message'
 import About from './Pages/About'
 import Home from './Pages/Home'
 import Post from './Pages/Post'
 import Layout from './Components/Layout'
 
-const App = () => {
-    const [show, setShow] = useState([])
+const App = () => (
+    <>
+        <div className="App">
+            <h1>Hello World</h1>
+        </div>
 
-    return (
-        <>
-            <div className="App">
-                <h1>Hello World</h1>
-            </div>
+        <Routes>
+            <Route element={<Layout />}>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/post/:id" element={<Post />} />
+            </Route>
 
-            <button onClick={() => setShow((p) => [...p, true])}>Message</button>
-            {show.map((state, i) => (
-                <Message
-                    key={i}
-                    isShow={state}
-                    setShow={setShow}
-                    indication="message-indication-success"
-                    iconClass="message-indication-success-icon"
-                    icon="icon-success"
-                    title="Success"
-                    description="Lorem ipsum dolor sit amet."
-                />
-            ))}
-
-            <Routes>
-                <Route element={<Layout />}>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/about" element={<About />} />
-                    <Route path="/post/:id" element={<Post />} />
-                </Route>
-
-            </Routes>
-        </>
-    )
-}
+        </Routes>
+    </>
+)
 export default App
