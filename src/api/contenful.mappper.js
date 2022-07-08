29fa70fc3b25abb4743c1473cd64fa99a9b7e496
MrStonkus <valdas@stonkus.lt>
2022-07-08 11:36:25 +0300

    update contentful api to get whole home page posts

--- src/api/contenful.mappper.js
+++ src/api/contenful.mappper.js
@@ -2,10 +2,20 @@ export class ContentfulResponseMapper {
     postsResponseToPostList = (response) => response.fields.posts.map((post) => ({
         id: post.sys.id,
         title: post.fields.title,
-        description: post.fields.description.content[0].content[0].value,
+        description: post.fields.description.content,
         imageUrl: post.fields.picture.fields.file.url,
         imageTitle: post.fields.picture.fields.title,
         createdAt: post.sys.createdAt,
         updatedAt: post.sys.updatedAt
     }))
+
+    wrappersToWrapperList = (wrappers) => {
+        const wrapperList = []
+        wrappers.forEach((wrapper) => {
+            wrapperList.push({
+                id: wrapper.sys.id
+            })
+        })
+        return wrapperList
+    }
 }
--- src/api/readme.md
+++ src/api/readme.md
@@ -1,16 +1,9 @@
 # Get demo posts
 
-#### Just modify index.js by adding this code:
-
-
 ```
 import PostsApi from './api/contentful.api'
-const posts=newPostsApi().getPosts()
-console.table(posts)
+const posts=new PostsApi().getPosts()
+console.log(posts)
 ```
-
-You will get demo posts from contentful in the website console as array of objects.
-
-P.S. See in promise results.
 
 More about contentful use you can find here: [https://www.npmjs.com/package/contentful](https://www.npmjs.com/package/contentful)
